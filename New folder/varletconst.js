09beee70631b27b503893b001f9699fe5e31648c
nadeem <nadeem9918@gmail.com>
2023-06-03 13:47:59 +0530

    var let const commit

---
+++ New folder/varletconst.js
@@ -0,0 +1,17 @@
+/*var fistname;
+var lastname;
+
+fistname = "nadeem";
+lastname = "khan";
+fistname = "faheem"
+document.write(fistname)
+document.write(lastname)
+*/
+
+/*let fistname = "nadeem"
+    fistname ="faheem"
+document.write(fistname)*/
+
+const fistname = "nadeem"
+//fistname = "faheem"
+document.write(fistname)
