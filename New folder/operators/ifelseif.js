524f179321d2903f4ad87193b8b9628f465049f9
nadeem <nadeem9918@gmail.com>
2023-06-06 16:21:49 +0530

    if else if

---
+++ New folder/operators/ifelseif.js
@@ -0,0 +1,6 @@
+var time =16
+
+if (time > 1 && time < 12){document.write ("good morning")}
+else if (time >11 && time <15){document.write("good after noon")}
+else if (time >15 && time <18){document.write("good evning")}
+else if (time >18 && time <24){document.write("good night")};
